1720c90f620854d06c96aa351abda7af097d5c98
Elton Leite Ayres de Medeiros Borges <elton-borges@elton>
2020-08-25 20:53:53 -0300

    :alembic: Cria o exemplo da aula 44

---
+++ src/main/java/tutorial1/ProducerDemo.java
@@ -0,0 +1,31 @@
+package tutorial1;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+
+    public static void main(String[] args) {
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(getProperties());
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic","name","Hello World");
+
+        producer.send(record);
+
+        producer.flush();
+        producer.close();
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
