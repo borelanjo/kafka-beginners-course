4a55c63a9a5c0b7eacd4f31655492c7cfd2836fe
Elton Leite Ayres de Medeiros Borges <elton-borges@elton>
2020-08-25 20:54:34 -0300

    :alembic: Cria exemplo da aula 45

---
+++ src/main/java/tutorial1/ProducerWithCallbackDemo.java
@@ -0,0 +1,44 @@
+package tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerWithCallbackDemo {
+
+    private static Logger logger = LoggerFactory.getLogger(ProducerWithCallbackDemo.class);
+
+    public static void main(String[] args) {
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(getProperties());
+
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "name", "Hello World");
+
+        producer.send(record, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                if (e != null) {
+                    logger.error("Ocorreu um erro", e);
+                    return;
+                }
+                logger.info("Mensagem consumida");
+
+
+            }
+        });
+
+        producer.flush();
+        producer.close();
+    }
+
+    private static Properties getProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
